9279a0aadc0e7fb7cc48f247d606b09c64420e0e
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2023-02-04 21:46:12 +0500

    project update

---
+++ TestProject/TestProject/FigureAreaCalculator.Tests/Figures/TriangleTests.cs
@@ -0,0 +1,54 @@
+namespace FigureAreaCalculator.Tests.Figures
+{
+    public class TriangleTests
+    {
+        [Fact]
+        public void CalculateAreaIsCorrect()
+        {
+            // Given
+            double a = 4;
+            double b = 6;
+            double c = 23;
+
+            var triangle = new Triangle(a, b, c);
+
+            // When
+            var result = triangle.CalculateArea();
+            
+            double p = (a + b + c) / 2;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            // Then
+            Assert.Equal(double.Round(s, 2), result);
+        }
+
+        [Fact]
+        public void IsRightAngledIsCorrect()
+        {
+            // Given
+            double a = 4;
+            double b = 6;
+            double c = 23;
+
+            var triangle = new Triangle(a, b, c);
+
+            // When
+            var result = triangle.IsRightAngled();
+
+            double[] areas = new double[] { a, b, c };
+            
+            double max = areas.Max();
+
+            double d = areas.First(x => x != max);
+            double e = areas.First(x => x != d && x != max);
+
+            bool rightAgled = false;
+
+             if(Math.Pow(max, 2) == (Math.Pow(d, 2) + Math.Pow(e, 2)))
+                rightAgled = true;
+
+            // Then
+            Assert.Equal(rightAgled, result);
+        }
+    }
+}
